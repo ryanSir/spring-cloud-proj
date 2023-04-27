9330e4f441bb62049cbd06a8de07662beb32b39d
ryanSir <1246120217@qq.com>
2023-04-27 13:49:22 +0800

    init

---
+++ src/main/java/com/ryan/Main.java
@@ -0,0 +1,13 @@
+package com.ryan;
+
+/**
+*
+*
+* @author ${USER}
+* @version Id: ${NAME}, v 0.1 ${DATE} ${TIME} ${USER} Exp $
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
